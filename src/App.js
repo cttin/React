141446c3993de8cb9641663b26f9a4b44202733c
cao tingting <995298776@qq.com>
2017-05-05 14:30:11 +0800

    '将输入框变成Todoput组件，将每个待办封装为TodoItem'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
 
 class App extends Component {
   constructor(props) {
@@ -7,19 +9,24 @@ class App extends Component {
     this.state = {
       newTodo: 'test',
       todoList: [
-        {id: 1, title: '第一个待办'}
+        {id: 1, title: '第一个待办'},
+        {id: 2, title: '第二个待办'}
       ]
     }
   }
   render() {
     let todos = this.state.todoList.map((item, index) => {
-      return <li>{item.title}</li>
+      return (
+        <li>
+          <TodoItem todo={item} />
+        </li>
+      )
     })
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <input type="text" value={this.state.newTodo} />
+          <TodoInput content={this.state.todoList} />
         </div>
         <ol>
         {todos}
--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import './index.css';
 
-// ReactDOM.render(
-//   <h1>Hello, React!</h1>,
-//   document.getElementById('root')
-// );
-
-// function tick() {
-// 	const element = (
-// 		<div>
-// 			<h1>Hello, world!</h1>
-// 			<h2>It is {new Date().toLocaleTimeString()}.</h2>
-// 		</div>
-// 	);
-// 	ReactDOM.render(
-// 		element,
-// 		document.getElementById('root')
-// 	);
-// }
-
-// setInterval(tick, 1000);
-
-// class Welcome extends React.Component {
-// 	render() {
-// 		return <h1>Hello, Component</h1>;
-// 	}
-// }
-import Welcome from './Welcome'
 
 ReactDOM.render(
-	<Welcome name="Christina"/>,
+	<App />,
 	document.getElementById('root')
 )
