5d94aa6f95b54c06e4cbe8ffd7a98ec104a4cf24
cao tingting <995298776@qq.com>
2017-05-07 15:04:33 +0800

    fix value again

--- src/App.js
+++ src/App.js
@@ -24,18 +24,23 @@ class App extends Component {
         </li>
       )
     })
-    console.log(todos);
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} onChange={this.changeTitle.bind(this)} />
         </div>
         <ol>
           {todos}
         </ol>
       </div>
     )
+  }
+  changeTitle(event) {
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
   }
   addTodo(event) {
     this.state.todoList.push({
